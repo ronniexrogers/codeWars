bde6578c956d88a2221c6f17b8401db89320bbc1
ronniexrogers <ronniexrogers@yahoo.com>
2022-12-07 09:21:20 -0800

    refactored code to be shorter

--- mathIssues.js
+++ mathIssues.js
@@ -1,13 +1,13 @@
-Math.round = (number) => {
-    return (number - ~~number >= .5 ? 1 : 0) + ~~number
-  }
-  
-Math.ceil = (number) => {
-    return ~~number === number ? number : ~~number + 1
-  }
-  
-Math.floor = (number) => {
-    return ~~number
-  }
+// Oh no, our Math object was "accidently" reset. Can you re-implement some of those functions? We can assure, that only non-negative numbers are passed as arguments. So you don't have to consider things like undefined, null, NaN, negative numbers, strings and so on.
 
-  console.log(Math.ceil(5))
+// Here is a list of functions, we need:
+
+// Math.round()
+// Math.ceil()
+// Math.floor()
+
+Math.round = (number) => (number - ~~number >= .5 ? 1 : 0) + ~~number
+  
+Math.ceil = (number) =>  ~~number === number ? number : ~~number + 1
+  
+Math.floor = (number) => ~~number
